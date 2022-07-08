0ed754f843bac9d1f210948ef5784dfcb611fd4d
Pabl0Parra <pabsblue16@gmail.com>
2022-07-08 18:19:00 +0200

    fixed unit test esLint error

--- src/__test__/ShipsList.test.js
+++ src/__test__/ShipsList.test.js
@@ -1,5 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import ShipsList from "../components/ShipsList";
+
+// --- Test Driven Dev -----
+// 1. RED (failed) --> create a test that fails
+// 2. GREEN (passed) --> write minimun code in order to pass the test
+// 3. Refactor --> apply clean code & other good practices (like code smells..)
 
 describe("Ships list mount", () => {
   it("must display the first 10 ships' names", async () => {
